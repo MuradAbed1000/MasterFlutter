cba8d12928a477cef25af6eae96ea9da8c1ecc63
Murad Abed <muradabed683@gmail.com>
2021-08-08 22:03:32 +0300

    day-11completed

--- master/lib/pages/home_page.dart
+++ master/lib/pages/home_page.dart
@@ -41,11 +41,43 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-            ? ListView.builder(
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16.0,
+                  crossAxisSpacing: 16.0,
+                ),
+                itemBuilder: (context, indext) {
+                  final item = CatalogModel.items![indext];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(item.name.toString(),style: TextStyle(color: Colors.white),),
+                          padding:const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        child: Image.network(item.image.toString()),
+                        footer: Container(
+                          child: Text(item.price.toString(),style: TextStyle(color: Colors.white),),
+                          padding:const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ));
+                },
                 itemCount: CatalogModel.items!.length,
-                itemBuilder: (context, index) =>
-                    ItemWidget(item: CatalogModel.items![index]),
               )
+            // ListView.builder(
+            //     itemCount: CatalogModel.items!.length,
+            //     itemBuilder: (context, index) =>
+            //         ItemWidget(item: CatalogModel.items![index]),
+            //   )
             : Center(
                 child: CircularProgressIndicator(),
               ),
